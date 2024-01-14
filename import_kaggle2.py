f6a86aac68bbcbcd95daf418f5b742d9465614a2
BASED1414 <svilcad@unsa.edu.pe>
2024-01-14 00:36:32 -0500

    archivos para carga en base de datos

---
+++ import_kaggle2.py
@@ -0,0 +1,36 @@
+import json
+import zipfile
+import os
+
+api_token ={"username":"sebastian1414","key":"bbe304671a529062dfaffc134da107d3"}
+
+##Conectar a Kaggle
+with open('C:/Users/Tu vieja/.kaggle/kaggle.json',"w") as file:
+    json.dump(api_token, file)
+
+location = "C:/Users/Tu vieja/Documents/Proyecto/dataset"
+
+##Validar que la carpeta exista
+if not os.path.exists(location):
+##Si no existe la carpeta dataset entonces la creo
+    os.mkdir(location)
+else:
+##Si la carpeta si existe, entonces voy a borrar su contenido
+    for root, dirs, files in os.walk(location, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root,name)) ##elimino todos los archivos
+        for name in dirs:
+            os.rmdir(os.path.join(root,name)) ##elimino todas las carpetas
+
+##Descargar dataset de Kaggle
+os.system("kaggle datasets download -d HENRYSHAN/Starbucks -p c:/Users/USER/Documents/Proyecto/dataset")
+
+##Descomprimir el archivo de Kaggle
+os.chdir(location)
+for file in os.listdir():
+    zip_ref = zipfile.ZipFile(file,"r") ##lee archivo .zip
+    zip_ref.extractall() ##extrae contenido de archivo .zip
+    zip_ref.close() ##cierra archivo
+
+
+
